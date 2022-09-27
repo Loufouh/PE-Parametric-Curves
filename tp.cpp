72a4a9930cb757a2b8fbbf30b030ba2fd3ca3b03
Louis Masson <louis.masson.contact@gmail.com>
2022-09-27 10:56:34 +0200

    :sparkles: Exercice 1.3 Create HermitCubicCurve and draw one

--- tp.cpp
+++ tp.cpp
@@ -85,23 +85,40 @@ void init() {
     glEnable(GL_COLOR_MATERIAL);
 }
 
+std::vector<Vec3> HermiteCubicCurve(Vec3 p0, Vec3 p1, Vec3 v0, Vec3 v1, long nbU) {
+    std::vector<Vec3> curvePoints;
+    curvePoints.reserve(nbU);
+
+    for(int i = 0; i < nbU; i++) {
+        float u = (float) i / (float) nbU;
+
+        float f1 = 2*u*u*u - 3*u*u + 1;
+        float f2 = -2*u*u*u + 3*u*u;
+        float f3 = u*u*u - 2*u*u + u;
+        float f4 = (u*u*u - u*u);
+
+        Vec3 point;
+
+        for(int j = 0; j < 3; j++) {
+            point[j] =  f1*p0[j] + f2*p1[j] + f3*v0[j] + f4*v1[j];
+        }
+
+        curvePoints.push_back(point);
+    }
+
+    return curvePoints;
+}
+
 std::vector<Vec3> curvePoints;
 
 void setupCurvePoints() {
-    curvePoints.clear();
-    curvePoints.reserve(100);
-
-    for(int i = 0; i < 100; i++) {
-        float x = (float) i / (float) 50 - 1;
-
-        curvePoints.push_back(
-            Vec3(
-                x,
-                x*x,
-                0
-            )
-        );
-    }
+    curvePoints = HermiteCubicCurve(
+        Vec3(0, 0, 0),
+        Vec3(2, 0, 0),
+        Vec3(1, 1, 0),
+        Vec3(1, -1, 0),
+        100
+    );
 }
 
 void update() {
